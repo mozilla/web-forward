ac3ac95703918a8dd329a2bdc91eb0d09c4a0563
Pascal Finette <pascal@finette.com>
2012-07-30 15:50:00 -0700

    Removed panel and OGV version of the video

--- site/panel/fields/checkbox/checkbox.php
+++
@@ -1,1 +0,0 @@
-<label class="inline input"><input type="checkbox" name="<?php echo html($name) ?>"<?php echo ($value === 'on' || $value === '1') ? 'checked' : ''?>> <?php echo html($label) ?></label>
