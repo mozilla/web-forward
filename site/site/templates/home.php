8ea8e7ef0bd349e200bc7c9aea3a02bbad5faea3
Pascal Finette <pascal@finette.com>
2012-10-22 14:57:00 -0700

    .

--- site/site/templates/home.php
+++ site/site/templates/home.php
@@ -13,10 +13,10 @@
 <!-- video -->
 <div class="row padding-small">
 	<div class="ten columns centered">
-		<video class="video-js vjs-default-skin" controls preload="auto" data-setup="{}" width="809" height="458" controls preload poster="https://webfwd.org/assets/images/webfwd_promotion.jpg">
-			<source src="https://webfwd.org/assets/images/webfwd_promotion.mp4" type='video/mp4'>
-			<source src="https://webfwd.org/assets/images/webfwd_promotion.webm" type='video/webm'>
-			<source src="https://webfwd.org/assets/images/webfwd_promotion.ogv" type="video/ogg" />
+		<video class="video-js vjs-default-skin" controls preload="auto" data-setup="{}" width="809" height="458" controls preload poster="https://videos.mozilla.org/uploads/webfwd/webfwd_promotion.jpg">
+			<source src="https://videos.mozilla.org/uploads/webfwd/webfwd_promotion.mp4" type='video/mp4'>
+			<source src="https://videos.mozilla.org/uploads/webfwd/webfwd_promotion.webm" type='video/webm'>
+			<source src="https://videos.mozilla.org/uploads/webfwd/webfwd_promotion.ogv" type="video/ogg" />
 		</video>
 	</div>
 </div>
