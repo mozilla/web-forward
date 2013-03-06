35fe055af573130b8386230381f21cf31dfe8415
Didem Ersoz <dersoz@mozilla.com>
2013-03-06 10:30:25 -0800

    added ogv video file

--- site/site/templates/about.php
+++ site/site/templates/about.php
@@ -13,7 +13,7 @@
 	<div class="ten columns centered">
 		<video class="video-js vjs-default-skin" controls preload="none" data-setup="{}" width="809" height="455" poster="/assets/images/webfwd_Feb_2013.jpg">
 			<source src="/assets/images/webfwd_Feb_2013.mp4" type='video/mp4' />
-			<!--<source src="/assets/images/webfwd_Feb_2013.ogv" type="video/ogg" /> -->
+			<source src="/assets/images/webfwd_Feb_2013.ogv" type="video/ogg" />
 			<source src="/assets/images/webfwd_Feb_2013.webm" type='video/webm' />
 		</video>
 	</div>
