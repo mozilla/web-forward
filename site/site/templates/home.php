f7eba1a4f3e53ac8b925b318d27a95b9e1d90e79
Didem Ersoz <dersoz@mozilla.com>
2014-01-10 14:25:28 -0800

    test

--- site/site/templates/home.php
+++ site/site/templates/home.php
@@ -6,7 +6,7 @@
 <div class="row padding" style="padding-bottom: 48px;">
 	<div class="twelve columns">
 
-		<h3 style="padding-left: 48px; line-height: 1.2;"> We are currently making some exciting changes for WebFWD. <a href="http://blog.webfwd.org/post/68820630391/moving-the-web-even-more-forward" target="_blank">Here</a> is a sneak peak of what is coming next. Continue following our blog for future updates.  </h3>
+		<h3> <b>We are currently making some exciting changes for WebFWD. <a href="http://blog.webfwd.org/post/68820630391/moving-the-web-even-more-forward" target="_blank">Here</a> is a sneak peak of what is coming next. Continue following our blog for future updates.  </b></h3>
 	</div>	
 </div>
 
