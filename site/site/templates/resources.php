110eb546d4b5b91f56f0fa1a86c829b281d6572b
Aaron Druck <aldruck@gmail.com>
2014-01-17 14:11:18 -0800

    updating the site

--- site/site/templates/resources.php
+++ site/site/templates/resources.php
@@ -24,26 +24,48 @@
   </section>
 
   <section class="row">
-    <div class="col-md-4">
-      <p></p>
+    <div class="col-xs-12 col-md-12">
+      <h3>Learning Modules</h3>
     </div>
-    <div class="col-md-4">
-      <p></p>
+    <div class="col-xs-6 col-md-4">
+      <div class="box">
+        <a href=""><img src="/assets/images/placeholder-400x300.png" class="img-responsive"></a>
+        <a href=""><p class="subtitle">Strategy</p></a>
+        <p class="author">Pascal Finette</p>
+        <p>Learn how to build an effective strategy to ensure your startup learns quickly and effectively to grow into a healthy business.</p>
+      </div>
     </div>
-    <div class="col-md-4">
-      <p></p>
+    <div class="col-xs-6 col-md-4">
+      <div class="box">
+        <a href=""><img src="/assets/images/placeholder-400x300.png" class="img-responsive"></a>
+        <a href=""><p class="subtitle">Strategy</p></a>
+        <p class="author">Pascal Finette</p>
+        <p>Learn how to build an effective strategy to ensure your startup learns quickly and effectively to grow into a healthy business.</p>
+      </div>
+    </div>
+    <div class="col-xs-6 col-md-4">
+      <div class="box">
+        <a href=""><img src="/assets/images/placeholder-400x300.png" class="img-responsive"></a>
+        <a href=""><p class="subtitle">Strategy</p></a>
+        <p class="author">Pascal Finette</p>
+        <p>Learn how to build an effective strategy to ensure your startup learns quickly and effectively to grow into a healthy business.</p>
+      </div>
+    </div>
+    <!-- Add the extra clearfix for only the required viewport -->
+    <div class="clearfix visible-xs"></div>
+    <div class="col-xs-6 col-md-4">
+      <div class="box">
+        <a href=""><img src="/assets/images/placeholder-400x300.png" class="img-responsive"></a>
+        <a href=""><p class="subtitle">Strategy</p></a>
+        <p class="author">Pascal Finette</p>
+        <p>Learn how to build an effective strategy to ensure your startup learns quickly and effectively to grow into a healthy business.</p>
+      </div>
     </div>
   </section>
 
   <section class="row">
-    <div class="col-md-4">
-      <p></p>
-    </div>
-    <div class="col-md-4">
-      <p></p>
-    </div>
-    <div class="col-md-4">
-      <p></p>
+    <div class="col-md-12">
+      <p>You can find old WebFWD resources <a href="/accelerator/about/resources/" target="_blank">here</a>.</p>
     </div>
   </section>
 
